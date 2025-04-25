cd3192ae4a4411d87e0d8abf76fa7b65f18ff1ac
Murali Elavarthi <muralis2p@gmail.com>
2025-04-25 12:29:36 +0530

    m1

--- terraform-aws-ec2-module/outputs.tf
+++ terraform-aws-ec2-module/outputs.tf
@@ -5,3 +5,5 @@ output "public_ip" {
 output "private_ip" {
     value = aws_instance.this.private_ip
 }
+
+# if we don't write output's in module, we can't fetch these values from project
--- test-project-ec2-module/ec2.tf
+++ test-project-ec2-module/ec2.tf
@@ -2,10 +2,11 @@ module "ec2" {
     source = "../terraform-aws-ec2"
     sg_id = "sg-0e978039cdb6c3057" # this one we are passing to module
     instance_type = "t3.small" # this one we are passing to module
+    #local.instance_type
 }
 
 output "public_ip" {
-   value =  module.ec2.public_ip
+   value =  module.ec2.public_ip # we are fetching this value from module
 }
 
 # "../terraform-aws-ec2" -- one step back, because ec2 module is in root folder.
